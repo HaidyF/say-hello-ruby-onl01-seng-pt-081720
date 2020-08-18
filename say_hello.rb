def9da3a92ec202789c698ca7f03afdddf961825
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-18 03:27:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,10 @@
 # Build your say_hello method here
-def say_hello(name="Ruby Programmer")
+def say_hello(name)
 puts "Hello #{name}!"  
 end
 say_hello("Kent Beck")
 
+def say_hello(name="Ruby Programmer")
+puts "Hello #{name}!"  
+end
+say_hello
